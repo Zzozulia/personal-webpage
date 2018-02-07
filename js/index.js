81633042591893f1d6da89ba920405fc889339c2
Yulia <yulia.zozulia13@gmail.com>
2018-02-07 14:24:36 +0000

    Changed

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+$("#contactform").submit(function(event) {
+
+var form_data = $(this).serialize();
+    event.preventDefault(); //prevent default action 
+		$.ajax({
+        url : "http://formspree.io/yulia.zeze@gmail.com",
+        type: "post",
+        data : form_data
+    }).done(function(response){ //
+        console.log(response)
+    });
+});
